3e982070aed7d428614b3c7b161b19ac7819269a
Thomas Francart <thomas.francart@sparna.fr>
2018-02-22 16:27:33 +0100

    Deleted datapress directory

--- fr.sparna/rdf/datapress/datapress-core/src/main/java/fr/sparna/rdf/datapress/DataPressListener.java
+++
@@ -1,9 +0,0 @@
-package fr.sparna.rdf.datapress;
-
-public interface DataPressListener {
-
-	public void begin(DataPressSource source);
-	
-	public void end(DataPressSource source, boolean success);
-	
-}
--- fr.sparna/rdf/datapress/datapress-core/src/main/java/fr/sparna/rdf/datapress/NotifyingDataPress.java
+++
@@ -1,9 +0,0 @@
-package fr.sparna.rdf.datapress;
-
-public interface NotifyingDataPress extends DataPress {
-
-	public void addListener(DataPressListener listener);
-	
-	public void removeListener(DataPressListener listener);
-	
-}
