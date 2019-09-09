73ff187acac4b1bec61a1f81fdf536dc9a19f901
Thomas Francart <thomas.francart@sparna.fr>
2019-09-09 22:26:45 +0200

    Fix #22 : move xls2rdf in its own repo + use jitpack for rdf4j-toolkit dependency

--- fr.sparna/rdf/xls2rdf/xls2rdf-app/src/main/java/fr/sparna/rdf/xls2rdf/app/CliCommandIfc.java
+++
@@ -1,8 +0,0 @@
-package fr.sparna.rdf.xls2rdf.app;
-
-
-public interface CliCommandIfc {
-
-	public void execute(Object args) throws Exception;
-	
-}
--- fr.sparna/rdf/xls2rdf/xls2rdf-lib/src/main/java/fr/sparna/rdf/xls2rdf/reconcile/ReconciliableValueSetIfc.java
+++
@@ -1,9 +0,0 @@
-package fr.sparna.rdf.xls2rdf.reconcile;
-
-import org.eclipse.rdf4j.model.IRI;
-
-public interface ReconciliableValueSetIfc {
-
-	IRI getReconciledValue(String value);
-
-}
